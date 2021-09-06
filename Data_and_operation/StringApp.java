656564febaa9df2526fca40fe7e36779991d92d7
lhs961021 <lhs961021@naver.com>
2021-09-06 16:13:55 +0900

    third

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,22 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		//Character VS String
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+		System.out.println("H"); // String
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		//new line
+		System.out.println("Hello \nWorld");
+		//Hello "World"
+		System.out.println("Hello \"World\""); //Hello "World"
+		
+		
+	}
+
+}
